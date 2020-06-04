631b0ca753ee71e43fde48a59596b668ba2bbd97
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-06-04 16:03:20 -0300

    First commit

---
+++ src/main/java/com/matheuscordeiro/myfinancesapi/MyFinancesApiApplication.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.myfinancesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFinancesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFinancesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheuscordeiro/myfinancesapi/MyFinancesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheuscordeiro.myfinancesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFinancesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
